52f40402e6b8fb2bc629e6ed08052a26f56d789e
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-04 15:17:41 +0600

    maximum 3 review show added

--- src/components/ReviewSection/ReviewSection.js
+++ src/components/ReviewSection/ReviewSection.js
@@ -1,20 +1,24 @@
 import useProduct from '../../hooks/useProduct';
 import SingleProduct from '../SingleProduct/SingleProduct';
+import './ReviewSection.css';
 
 const ReviewSection = () => {
   //custom hooks
-  const [products, setProducts] = useProduct();
+  const [products] = useProduct();
+  const myProducts = products.slice(0, 3);
 
   return (
-    <div className='bg-indigo-700'>
-      <h2 className='text-5xl text-center text-slate-300 font-mono'>Customer Reviews ({products.length})</h2>
-      <div className="reviews grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center my-28 text-center gap-5 px-5">
-        {
-          products.map(product => <SingleProduct 
-          key = {product._id}
-          product = {product}
-          />)
-        }
+    <div className="bg-indigo-700">
+      <h2 className="text-5xl text-center text-slate-300 font-mono">
+        Customer Reviews ({myProducts.length})
+      </h2>
+      <div className="reviews grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center mt-28 mb-10 text-center gap-5 px-5">
+        {myProducts.map((product) => (
+          <SingleProduct key={product._id} product={product} />
+        ))}
+      </div>
+      <div className="text-center pb-10">
+        <button className="see-more-btn text-3xl text-gray-200 px-10 py-1">See All Reviews</button>
       </div>
     </div>
   );
--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -4,14 +4,22 @@ import './SingleProduct.css';
 const SingleProduct = (props) => {
   const { name, picture, ratings, review } = props.product;
   return (
-    <div className='customer-review flex justify-center items-center'>
-      <div className="review-content rounded-2xl p-5 h-[400px]">
-        <img className="w-28 h-28 rounded-3xl mx-auto" src={picture} alt="" />
-        <h2 className='py-2 font-mono text-slate-300 text-xl'>Name: {name} </h2>
-        <p className='py-5 text-gray-400'>{review.slice(0,200)}</p>
-        <p><span className='font-medium text-orange-300'>Ratings: {ratings} stars</span></p>
+    <>
+      <div className="customer-review flex justify-center items-center">
+        <div className="review-content rounded-2xl p-5 h-[400px]">
+          <img className="w-28 h-28 rounded-3xl mx-auto" src={picture} alt="" />
+          <h2 className="py-2 font-mono text-slate-300 text-xl">
+            Name: {name}{' '}
+          </h2>
+          <p className="py-5 text-gray-400">{review.slice(0, 200)}</p>
+          <p>
+            <span className="font-medium text-orange-300">
+              Ratings: {ratings} stars
+            </span>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
